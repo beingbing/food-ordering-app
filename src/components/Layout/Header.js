1d55217b0337ee26773baca5c288035c87a4fc04
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-19 04:33:44 +0530

    header component with css and images added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="A Table full of food" />
+            </div>
+        </Fragment>
+    )
+};
+
+export default Header;
