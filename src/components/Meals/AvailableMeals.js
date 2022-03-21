79e62910625d368ac330d480ee097c838aaf0d25
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2022-03-21 18:28:21 +0530

    fetching meals data, and check for any error in fetching with loading feature added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,13 +4,15 @@ import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://react-http-b9f99-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json');
+      if (!response.ok) throw new Error('Something went wrong!');
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -23,12 +25,32 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
-
+      console.log('loaded meals: ', loadedMeals);
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
